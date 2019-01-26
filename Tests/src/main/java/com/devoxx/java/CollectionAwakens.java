1113c747f56f29d516b8c9ef372abf082e83a5ca
AnekGit <anekkumarsingh01@outlook.com>
2019-01-26 12:26:27 +0530

    ConcurrentMOdification Exception

--- Tests/src/main/java/com/devoxx/java/CollectionAwakens.java
+++ Tests/src/main/java/com/devoxx/java/CollectionAwakens.java
@@ -1,9 +1,23 @@
 package com.devoxx.java;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Syntax.MetaCharTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 public class CollectionAwakens {
     public static void main(String... args){
 
         System.out.println("Here is my code ");
+
+        List<String> jedis = new ArrayList(asList("Anek","roul"));
+
+        for(String jedi : jedis){
+            if(Character.isLowerCase(jedi.charAt(0)))
+                jedis.remove(jedi);
+            System.out.println(jedi);
+        }
 
     }
 }
